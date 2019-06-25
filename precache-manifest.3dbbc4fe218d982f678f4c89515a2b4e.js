2db5cc605361eb32cf6f1886ad5fa2285bc2ba11
Angela Cullen <esmearmitage@gmail.com>
2019-06-25 19:39:56 +0100

    Updates

---
+++ precache-manifest.3dbbc4fe218d982f678f4c89515a2b4e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19d513780d0b5e6570fe6ed19f3b1995",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "d6e38924f0a62112dcc7",
+    "url": "/index.html/static/js/2.371c251d.chunk.js"
+  },
+  {
+    "revision": "9ff0aa232181ec9ad6f2",
+    "url": "/index.html/static/js/main.8ff92c27.chunk.js"
+  },
+  {
+    "revision": "5f361fe7f248455b5c92",
+    "url": "/index.html/static/js/runtime~main.01dc9df8.js"
+  }
+]);
