db61b95047b8d5db198f15e16eab180a59e2a161
Angela Cullen <angela.cullen@ynap.com>
2019-07-12 17:06:19 +0100

    fixup! manually deploy from react public folder

---
+++ precache-manifest.c2d2ca4ec62978def163ef7baf3990ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0ee1375cd12c1271df9968ece1d075e",
+    "url": "/index.html/index.html"
+  },
+  {
+    "revision": "d6e38924f0a62112dcc7",
+    "url": "/index.html/static/js/2.371c251d.chunk.js"
+  },
+  {
+    "revision": "215b441aadc5c1788000",
+    "url": "/index.html/static/js/main.eb91c42d.chunk.js"
+  },
+  {
+    "revision": "5f361fe7f248455b5c92",
+    "url": "/index.html/static/js/runtime~main.01dc9df8.js"
+  }
+]);
